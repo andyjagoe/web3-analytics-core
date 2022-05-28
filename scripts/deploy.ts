c031af9c00adbb517ace4228c2968579a8f3e8d9
Andy Jagoe <andy@jagoe.com>
2022-05-27 18:20:57 -0700

    fix hardhat network and add faucet script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,12 +4,12 @@ async function main() {
   await run('compile');
 
   // Rinkeby account addresses
-  //const trustedForwarder = '0x83A54884bE4657706785D7309cf46B58FE5f6e8a';
-  //const relayHub = '0x6650d69225CA31049DB7Bd210aE4671c0B1ca132';
+  const trustedForwarder = '0x83A54884bE4657706785D7309cf46B58FE5f6e8a';
+  const relayHub = '0x6650d69225CA31049DB7Bd210aE4671c0B1ca132';
 
   // Hardhat account addresses
-  const trustedForwarder = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
-  const relayHub = '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9';
+  //const trustedForwarder = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
+  //const relayHub = '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9';
 
 
   // We get the contract to deploy
